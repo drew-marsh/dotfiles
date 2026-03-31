7ed4342f09a16750e7b22d18fc1523d3edc9140a
Drew Marshall <thedrewguy@gmail.com>
2026-03-31 12:12:23 -0700

    make update script pull git

--- powershell/profile/update.ps1
+++ powershell/profile/update.ps1
@@ -1,12 +1,12 @@
 function updateProfile {
-  $PSCommandPath
   $symlinkTarget = (get-item $PSCommandPath).ResolveLinkTarget($true).FullName
   $actualPath = $PSCommandPath
   if ($symlinkTarget) {
     $actualPath = $symlinkTarget
   }
 
-  $psDir = ($actualPath | Split-Path -Parent | Split-Path -Parent)
-  $initPath = join-path $psDir init.ps1
+  $dotfiles = ($actualPath | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent)
+  $initPath = join-path $dotfiles powershell/init.ps1
+  git -C $dotfiles pull
   & $initPath
 }
